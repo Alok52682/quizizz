6dc0febf2241567a6971503267bcba43a9983836
Alok <alok@Aloks-MacBook-Air.local>
2022-10-11 10:35:09 +0600

    show correct ans and eye icon implemented

--- src/Components/Option.js
+++ src/Components/Option.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Option = ({ ans, handleAnswer }) => {
     return (
-        <div onClick={() => handleAnswer(ans)} className=' bg-red-50 rounded-lg cursor-pointer m-5 w-9/12 mx-auto py-3 shadow-inner hover:border hover:border-red-400'>
-            <li className='text-start w-2/4 mx-auto'>{ans}</li>
+        <div onClick={() => handleAnswer(ans)} className=' bg-red-50 rounded-lg cursor-pointer m-5 w-11/12 md:w-9/12 mx-auto p-3 shadow-inner hover:border hover:border-red-400'>
+            <li className='text-start w-9/12 mx-auto'>{ans}</li>
         </div>
     );
 };
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,8 +1,12 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Option from './Option';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react';
 
 const Question = ({ ques }) => {
+    const [open, setOpen] = useState(false);
+
     const handleAnswer = (ans) => {
         if (ans === ques.correctAnswer) {
             toast.success("Answer is currect!!");
@@ -14,12 +18,19 @@ const Question = ({ ques }) => {
     return (
         <div className='w-9/12 mx-auto border border-error rounded-lg shadow-lg shadow-slate-300/50 p-5 my-10'>
             <h1 className='text-xl font-semibold'>{ques.question}</h1>
+
             <ol className='list-decimal'>
                 {
                     ques.options.map((ans, index) => <Option key={index} ans={ans} handleAnswer={handleAnswer} />)
                 }
                 <ToastContainer />
             </ol>
+            <div onClick={() => setOpen(!open)} title='Show Currect Answer' className='cursor-pointer'>
+                {open ? <EyeSlashIcon className="h-6 w-6" /> : <EyeIcon className="h-6 w-6" />}
+            </div>
+            {
+                open ? <p className='font-bold text-success'>Correct Answer : {ques.correctAnswer}</p> : undefined
+            }
         </div>
     );
 };
